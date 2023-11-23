40139d0bbee54c2185e4cea4ab4d9af4eece50de
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-11-23 17:54:34 +0700

    [feat] add authentication token middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+
+const tokenValidator = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    response.status(401).json({
+      status: false,
+      error: 'unauthorized access',
+    })
+    return
+  }
+  const token = authorization.replace('Bearer ', '')
+
+  let decodedToken = {}
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  } catch (error) {
+    response.status(401).json({
+      status: false,
+      error: 'Invalid token',
+      message: error.message,
+    })
+    return
+  }
+
+  if (!decodedToken.username) {
+    response.status(401).json({
+      status: false,
+      error: 'unauthorized access',
+    })
+    return
+  }
+
+  request.username = decodedToken.username
+  next()
+}
+
+module.exports = {
+  tokenValidator,
+}
