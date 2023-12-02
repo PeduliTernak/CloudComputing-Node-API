0585706cfcbeb7dedc743e17dfcf22ad96ba7c61
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-12-02 16:57:50 +0700

    [refactor] refactor handler

--- middleware/cloudStorage.js
+++ middleware/cloudStorage.js
@@ -5,40 +5,32 @@ const { BUCKET } = require('../utils/config')
 const bucket = storage.bucket(BUCKET)
 
 const uploadImage = async (request, response, next) => {
-  try {
-    // Create Unique ID and File Name for Object
-    const id = generateRandomString(25)
-    const fileName = `${request.user.username}-${id}-${request.file.originalname}`
-    const file = bucket.file(fileName)
+  // Create Unique ID and File Name for Object
+  const id = generateRandomString(25)
+  const fileName = `${request.user.username}-${id}-${request.file.originalname}`
+  const file = bucket.file(fileName)
 
-    // Write Stream
-    const stream = file.createWriteStream({
-      metadata: {
-        contentType: request.file.mimetype,
-      },
-    })
-    stream.end(request.file.buffer)
+  // Write Stream
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: request.file.mimetype,
+    },
+  })
+  stream.end(request.file.buffer)
 
-    // Wait for the file to be fully uploaded
-    await new Promise((resolve, reject) => {
-      stream.on('finish', resolve)
-      stream.on('error', reject)
-    })
+  // Wait for the file to be fully uploaded
+  await new Promise((resolve, reject) => {
+    stream.on('finish', resolve)
+    stream.on('error', reject)
+  })
 
-    // Store data to response.locals
-    response.locals.storageObject = {
-      id,
-      fileName,
-      publicUrl: `https://storage.googleapis.com/${BUCKET}/${fileName}`,
-    }
-    next()
-  } catch (error) {
-    console.error(error)
-    response.status(500).json({
-      success: false,
-      message: `Internal Server Error: ${error.message}`,
-    })
+  // Store data to response.locals
+  response.locals.storageObject = {
+    id,
+    fileName,
+    publicUrl: `https://storage.googleapis.com/${BUCKET}/${fileName}`,
   }
+  next()
 }
 
 module.exports = {
--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -6,29 +6,39 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-  if (error.name === 'JsonWebTokenError') {
-    response.status(400).json({
-      status: false,
-      error: error.message,
-    })
-  } else if (error.name === 'TokenExpiredError') {
-    response.status(400).json({
-      status: false,
-      error: 'token expired',
-    })
-  } else if (error.name === 'MulterError') {
-    if (error.message === 'Unexpected field') {
-      response.status(400).json({
-        status: false,
-        error: 'please specify form-data with \'file\' key',
-      })
-    } else if (error.message === 'File too large') {
-      response.status(400).json({
-        status: false,
-        error: 'maximum file is 3MB',
-      })
-    }
+  const errorMappings = {
+    JsonWebTokenError: {
+      status: 400,
+      message: error.message,
+    },
+    TokenExpiredError: {
+      status: 400,
+      message: 'token expired',
+    },
+    MulterError: {
+      'Unexpected field': {
+        status: 400,
+        message: 'please specify form-data with \'file\' key',
+      },
+      'File too large': {
+        status: 400,
+        message: 'maximum file size is 3MB',
+      },
+    },
   }
+
+  let errorInfo = errorMappings[error.name] || { status: 500, message: 'Internal Server Error' }
+  errorInfo = errorInfo[error.message] || errorInfo
+
+  if (errorInfo.status === 500) {
+    console.error(error)
+  }
+
+  response.status(errorInfo.status).json({
+    status: false,
+    error: errorInfo.message,
+  })
+
   next(error)
 }
 
